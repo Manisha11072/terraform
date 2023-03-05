710709dc0557cc6b2a2f52c30aa725000809fc47
Geethika <manishanetha20000@gmail.com>
2023-03-05 11:25:48 -0600

    terraform

---
+++ untitled/sample/test.tf
@@ -0,0 +1,8 @@
+resource "amazon_instnces" "ec2" {
+  ami = "ami-0a017d8ceb274537d"
+  instance-type = "t2.micro"
+  vpc_security_groups_ids = ["sg-031349396f653361e"]
+ tags = {
+   name = "test"
+ }
+}
